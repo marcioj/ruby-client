5c5c84a3e7e7b41b281560e30119498d27eaed9a
Marcio Junior <marciojunior_eu@yahoo.com.br>
2014-09-17 17:24:04 -0300

    Update tests to use rspec

--- spec/blockcypher/api_spec.rb
+++ spec/blockcypher/api_spec.rb
@@ -1,58 +1,58 @@
-describe BlockCypher::Api do
-  let(:api) { BlockCypher::Api.new(currency: BlockCypher::BTC, network: BlockCypher::TEST_NET_3, version: BlockCypher::V1) }
+describe BlockCypher::Api, :vcr do
+  subject do
+    BlockCypher::Api.new(currency: BlockCypher::BTC, network: BlockCypher::TEST_NET_3, version: BlockCypher::V1)
+  end
 
-  let(:address_1) { 'miB9s4fcYCEBxPQm8vw6UrsYc2iSiEW3Yn' }
-  let(:address_1_private_key) { 'f2a73451a726e81aec76a2bfd5a4393a89822b30cc4cddb2b4317efb2266ad47' }
-
-  let(:address_2) { 'mnTBb2Fd13pKwNjFQz9LVoy2bqmDugLM5m' }
-  let(:address_2_private_key) { '76a32c1e5b6f9e174719e7c1b555d6a55674fdc2fd99cfeee96a5de632775645' }
+  let(:address_1) { "n2JLznPZCUnLRmSLzr9UAE22psexCVdz8i" }
+  let(:address_2) { "mxZUdSxjxFZvmVpo2NuLSQ1CZZonhns8w5" }
 
   let(:token) { 'testtoken' }
 
-  describe '#transaction_new' do
+  # describe '#transaction_new' do
+  #
+  #   it 'should call the txs/new api' do
+  #     input_addresses = [address_1]
+  #     output_addresses = [address_2]
+  #     satoshi_value = 20000
+  #     res = api.transaction_new(input_addresses, output_addresses, satoshi_value)
+  #     expect(res["tx"]["hash"]).to be_present
+  #   end
+  # end
+  #
+  # describe '#transaction_sign_and_send' do
+  #
+  #   it 'should call txs/send api' do
+  #
+  #     input_addresses = [address_2]
+  #     output_addresses = [address_1]
+  #     satoshi_value = 10000
+  #
+  #     new_tx = api.transaction_new(input_addresses, output_addresses, satoshi_value)
+  #     res = api.transaction_sign_and_send(new_tx, address_2_private_key)
+  #     expect(res["tx"]["hash"]).to be_present
+  #   end
+  #
+  # end
 
-    it 'should call the txs/new api' do
-      input_addresses = [address_1]
-      output_addresses = [address_2]
-      satoshi_value = 20000
-      res = api.transaction_new(input_addresses, output_addresses, satoshi_value)
-      expect(res["tx"]["hash"]).to be_present
+  describe "#address_final_balance" do
+    let!(:balance_1) { subject.address_final_balance(address_1) }
+    let!(:balance_2) { subject.address_final_balance(address_2) }
+
+    it "gets the balance of address 1" do
+      expect(balance_1).to eq(10000000)
+    end
+
+    it "gets the balance of address 2" do
+      expect(balance_2).to eq(12000000)
     end
   end
 
-  describe '#transaction_sign_and_send' do
-
-    it 'should call txs/send api' do
-
-      input_addresses = [address_2]
-      output_addresses = [address_1]
-      satoshi_value = 10000
-
-      new_tx = api.transaction_new(input_addresses, output_addresses, satoshi_value)
-      res = api.transaction_sign_and_send(new_tx, address_2_private_key)
-      expect(res["tx"]["hash"]).to be_present
+  describe "#event_webhook_subscribe"  do
+    let!(:hook_response) do
+      subject.event_webhook_subscribe("http://requestb.in/yvrbljyv", "tx-confirmation", token, address: address_1)
     end
 
+    it { expect(hook_response["event"]).to eq("tx-confirmation") }
+    it { expect(hook_response["url"]).to eq("http://requestb.in/yvrbljyv") }
   end
-
-  describe '#address_final_balance' do
-
-    it 'should get the balance of an address' do
-      balance = api.address_final_balance(address_1)
-      expect(balance).to be_kind_of Integer
-      expect(balance).to be > 0
-    end
-
-  end
-
-  describe '#event_webhook_subscribe'  do
-
-    it 'should create a tx-confirmation hook ' do
-      hook_response = api.event_webhook_subscribe('http://requestb.in/yvrbljyv', 'tx-confirmation', token, address: 'mjU2XANrziNRB8T2vGPEvPRagY1uxwRzyT')
-      expect(hook_response['event']).to eq('tx-confirmation')
-      expect(hook_response['url']).to eq('http://requestb.in/yvrbljyv')
-    end
-
-  end
-
 end
